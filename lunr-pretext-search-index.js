9b481766d9026221eb937b5dd4425ef7bd7aa6ba
olidec <69003898+olidec@users.noreply.github.com>
2023-09-05 13:13:44 +0200

    Latest build deployed.

---
+++ lunr-pretext-search-index.js
@@ -0,0 +1,112 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "algebra",
+  "level": "1",
+  "url": "algebra.html",
+  "type": "Section",
+  "number": "1",
+  "title": "Algebra",
+  "body": " Algebra   The Big Ideas in Algebra  The following graphic tries to give you an overview over the, so called, big ideas in algebra. Over the next four years we will cover these and many more topics.   What is Algebra?  In its most general form, algebra is the study of mathematical symbols and the rules for manipulating these symbols. It is a unifying thread of almost all of mathematics. (…) The more basic parts of algebra are called elementary algebra, the more abstract parts are called abstract algebra or modern algebra. Elementary algebra is essential for any study of mathematics, science, or engineering, as well as such applications as medicine and economics. Abstract algebra is a major area in advanced mathematics, studied primarily by professional mathematicians. Much early work in algebra, as the Arabic origin of its name suggests, was done in the Near East, by such mathematicians as al-Khwārizmı̄ (780 – 850) and Omar Khayyam (1048–1131)    The Building Blocks of Algebra  The main building blocks of algebra are variables , expressions and equations .   Variables  Variables can have different interpretations depending on the situation:    In an equation (see below) a variable may stand for an unknown number:     A variable may be used to describe a general situation: If a car is going 5 m\/s then after seconds it will have traveled meters.    Variables can describe relationships between quantities that can vary: If is the circumference of a circle and is its diameter, then the ratio is always constant and .    Variables can be used to describe general rules (or theorems) of mathematics: The operation addition is commutative i.e. .      Expressions  For example,    Added terms are simplified using coefficients. For example, can be simplified as (where 3 is the coefficient).    Multiplied terms are simplified using exponents. For example, is represented as     Like terms are added together, for example, is written as , because the terms containing are added together, and, the terms containing are added together.    Brackets can be \"multiplied out\", using distributivity. For example, can be written as which can be written as .    Expressions can be factored. For example, , by dividing both terms by can be written as      Evaluate the following terms for the given -values.    some expressions                                                 Equations    Some equations are true for all values of the involved variables: The above example of the commutativity of addition states that for all values of and .    Some equations are only true for certain values: is only true if or if .    You can think of an equation as a balance: or   Solve the equations for each variable.   3                                  =-.75cm       A short list of Mathematical Expressions and Symbols      the set of the natural numbers: either or depending on the situation.     the set of the integers:     the set of the rational numbers (all fractions):     the set of the real numbers: contains all numbers on the number line.     the set of the complex numbers: adds to the real numbers. All algebraic equations can be solved in .     equals sign : the expression on the left is equal to the expression on the right.     plus sign : the result of an addition is a sum .     minus sign : the result of an subtraction is a difference .    or  times sign or multiplication sign : the result of an multiplication is a product .     division sign : the result of an division is a quotient .     fraction : an alternative method of writing division, consisting of a numerator ( ) and a denominator ( ).     the square root of ist the positive number such that .      squared : an abbreviation for . The general form is pronounced to the -th power .     less than : the expression on the left is less than the expression on the right.     less than or equal to : the expression on the left is less than or equal to the expression on the right.     greater than : the expression on the left is greater than the expression on the right.     greater than or equal to : the expression on the left is greater than or equal to the expression on the right.     brackets : are used to bring structure to expressions. they give the order in which calculations should be made.         Numbers and Expressions  The most fundamental building blocks in mathematics are numbers and the operations that can be performed on them. Algebra, like arithmetic, involves performing operations such as addition, subtraction, multiplication and division on numbers. In arithmetic, we are performing operations on known, specific, numbers (e.g. ). However, in algebra we often deal with operations on unknown numbers represented by variables usually symbolized by a letter (e .g. ). The use of variables gives us the power to write general statements indicating relationships between numbers.   Real Numbers  – Leopold Kronecker   A real number is any number that can be represented by a point on the real number line.     image   Each point on the real number line corresponds to one and only one real number, and each real number corresponds to one and only one point on the real number line. This kind of relationship is called a one-to-one correspondence. The number associated with a point on the real number line is called the coordinate of the point.   Just for Fun  source: http:\/\/xkcd.com\/899\/     Scientific Notation  A light year (the distance that light travels in one year) is 9 460 730 472 581 kilometres, and the mass of a single water molecule is 0.000 000 000 000 000 000 000 0056 grams. Without scientific notation these numbers seem meaningless. We have difficulty processing very large or very small numbers. However, if we were to write the numbers as km or g, they immediately become more clear.    we also immediately have a possibility for comparing numbers: If we change the exponent to , we say the number is now one order of magnitude (or ten times) larger.  For some useful applications of rounding and significant figures see:    http:\/\/www.youtube.com\/watch?v=hQpQ0hxVNTg   (link checked: 2.8.2023)   Write each number in scientific notation, rounding to 3 significant figures.   2    253.8    0.00781    7405239    0.000 001 0448    4.9812    0.001 991    Land area of Earth: 148 940 000 square kilometres    Relative density of hydrogen: 0.000 0899 grams per cm       Just for Fun    A man walks into a bar and says: I’d like ten times as many drinks as everyone here. – Now that’s an order of magnitude!     Abolute Value  The absolute value of a number n is the distance of the number n from zero. The absolute value is denoted by vertical bars as | n |, and is read aloud as \"the absolute value of n\". The formal definition of the absolute value is below     Evaluate each absolute value expression.    3                     Find all values of that make the equation true.    4                Roots and Radicals  The square root of a positive real number is a positive real number such that Higher roots are defined analogously: The -th root of a postive real number is a positive real number such that   The following rules apply for and :   Express each in terms of the simplest possible radical.    3                            Order of Operations  When algebraic expressions contain numerous operations, it is important to evaluate the operations in the proper order. Parentheses ( ), brackets [ ], and braces { } are used for grouping numbers and algebraic expressions. It is important to recognize that operations must be done first within parentheses and other grouping symbols. Other grouping symbols include absolute value bars, radical signs, and fraction bars.   Simplify the following expressions.   2                           Solving Equations   Solve the following equations.   2                         Fill in the gaps.                         Solve the equations. Do a substitution test.   2                                    Multiplying and Factoring Algebraic Expressions   Multiplying Algebraic Expressions  In special cases the formulas are particularly important:    Factoring Algebraic Expressions   Method 1: Common Factor    Method 2: Trial and Error    Application: Solving Equations    Expand and simplify.    3                     Factorize the expressions.    2                                 Solve the following equations    2                                   Working with Fractions  A fraction  is simply an alternative way of writing the division . If you remember the following, then you should be set:    In a fraction the upper number (or expression) is called the numerator .    The lower number (or expression) is called the denominator .    A fraction can be simplified if and only if the numerator and denominator share a common factor (e.g. ).    The minus sign of a negative fraction can be in many different places:     Fractions can be added and subtracted only if they have a common denominator:     Sometimes it is useful to split a fraction in multiple summands:      Simplify the following fractions   3                                   Simplify the following fractions.   3                                   Simplify the following fractions.   3                                   Multiply and simplify.   3                                   Divide and simplify.   2                                   Add\/subtract and simplify.   3                                   Simplify the following fractions    2               Perform the indicated operation and simplify.    2               Calclulate the sum.    Calculate.     Solving Word Problems  A classic math word problem consists of two steps:     Translate the problem into an equation.     Solve the equation.    As we gather more experience we will see that it is often necessary to add a third step to this process:     Assess the solution: Do the numbers make sense? What consequences does this result have? etc.    Even further along we will add an additional step at the very beginning:    Create a model that describes a given situation.    I hope you’ll agree with me that the second step is the most boring one here. As a result the other steps will become more and more prominent as we progress.   Translate the following into an equation and solve.    Four times a number is .    A number doubled and reduced by equals .    The sum of a number and its half equals .     To do a rope trick, a magician needs to cut a piece of rope so that one piece is one-third the length of the other piece. If she begins with a ft rope, what lengths will the two pieces of rope be?   Of three consecutive even integers, the sum of the smallest two integers is equal to 6 less than the largest. Find the integers.   Pat averages a rate of 11 mph on his bike. One day he rode for 45 min ( hr) and then got a flat tire and had to walk back home. He walked the same path that he rode and it took him 2 hr. What was his average rate walking?   Sharyn invests $2000 more in an account that earns 9% simple interest than she invests in an account that earns 6% simple interest. How much did she invest in each account if her total interest is $405 after 1 year?   In 2003, approximately 7.2 million men were in college in the United States.This represents an 8% increase over the number of men in college in 2000. Approximately how many men were in college in 2000? (Round to the nearest tenth of a million.)   In 2002, there were 17’430 deaths due to alcohol related accidents in the United States. This was a 5% increase over the number of alcohol-related deaths in 1999. How many such deaths were there in 1999?   Which is a better fit, a square peg in a round hole or a round peg in a square hole?   Take any two digit number, reverse its digits, and subtract the smaller number from the larger. Do you ever end up with a prime number?   How many numbers from 1 to 100 can you make, using the digits 2, 0, 2, 3 exactly once. You can use any operations you want (addition, multiplication, powers, roots, etc.)    Solutions    "
+},
+{
+  "id": "table-1",
+  "level": "2",
+  "url": "algebra.html#table-1",
+  "type": "Table",
+  "number": "1.1",
+  "title": "some expressions",
+  "body": " some expressions                                              "
+},
+{
+  "id": "table-2",
+  "level": "2",
+  "url": "algebra.html#table-2",
+  "type": "Table",
+  "number": "1.2",
+  "title": "",
+  "body": "    A short list of Mathematical Expressions and Symbols      the set of the natural numbers: either or depending on the situation.     the set of the integers:     the set of the rational numbers (all fractions):     the set of the real numbers: contains all numbers on the number line.     the set of the complex numbers: adds to the real numbers. All algebraic equations can be solved in .     equals sign : the expression on the left is equal to the expression on the right.     plus sign : the result of an addition is a sum .     minus sign : the result of an subtraction is a difference .    or  times sign or multiplication sign : the result of an multiplication is a product .     division sign : the result of an division is a quotient .     fraction : an alternative method of writing division, consisting of a numerator ( ) and a denominator ( ).     the square root of ist the positive number such that .      squared : an abbreviation for . The general form is pronounced to the -th power .     less than : the expression on the left is less than the expression on the right.     less than or equal to : the expression on the left is less than or equal to the expression on the right.     greater than : the expression on the left is greater than the expression on the right.     greater than or equal to : the expression on the left is greater than or equal to the expression on the right.     brackets : are used to bring structure to expressions. they give the order in which calculations should be made.    "
+},
+{
+  "id": "figure-1",
+  "level": "2",
+  "url": "algebra.html#figure-1",
+  "type": "Figure",
+  "number": "1.3",
+  "title": "",
+  "body": "  image  "
+},
+{
+  "id": "p-112",
+  "level": "2",
+  "url": "algebra.html#p-112",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Method 1: Common Factor "
+},
+{
+  "id": "p-113",
+  "level": "2",
+  "url": "algebra.html#p-113",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Method 2: Trial and Error "
+},
+{
+  "id": "p-114",
+  "level": "2",
+  "url": "algebra.html#p-114",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Application: Solving Equations "
+},
+{
+  "id": "p-148",
+  "level": "2",
+  "url": "algebra.html#p-148",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "factor "
+},
+{
+  "id": "p-224",
+  "level": "2",
+  "url": "algebra.html#p-224",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Translate Solve "
+},
+{
+  "id": "p-228",
+  "level": "2",
+  "url": "algebra.html#p-228",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Assess "
+},
+{
+  "id": "p-231",
+  "level": "2",
+  "url": "algebra.html#p-231",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "model "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
